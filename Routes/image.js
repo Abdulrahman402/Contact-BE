0f9d3ce05bfe2435283da5aefcbd3af3a539aca1
Abdulrahman <abduwemoh@gmail.com>
2020-03-22 20:18:20 +0200

    fixing cors

--- Routes/image.js
+++ Routes/image.js
@@ -1,87 +1,87 @@
-const express = require("express");
-const multer = require("multer");
-const _ = require("lodash");
+// const express = require("express");
+// const multer = require("multer");
+// const _ = require("lodash");
 
-const router = express.Router();
+// const router = express.Router();
 
-const { User } = require("../Models/User");
-const auth = require("../Middleware/auth");
+// const { User } = require("../Models/User");
+// const auth = require("../Middleware/auth");
 
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, "Image");
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: function(req, file, cb) {
+//     cb(null, "Image");
+//   },
+//   filename: function(req, file, cb) {
+//     cb(null, file.originalname);
+//   }
+// });
 
-const upload = multer(
-  {
-    storage: storage,
-    limits: {
-      fileSize: 3000000
-    },
-    fileFilter(req, file, cb) {
-      if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-        return cb(new Error("Please upload an image"));
-      }
-      cb(undefined, true);
-    }
-  },
-  (error, req, res, next) => {
-    res.status(404).send({ error: error.message });
-  }
-);
+// const upload = multer(
+//   {
+//     storage: storage,
+//     limits: {
+//       fileSize: 3000000
+//     },
+//     fileFilter(req, file, cb) {
+//       if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+//         return cb(new Error("Please upload an image"));
+//       }
+//       cb(undefined, true);
+//     }
+//   },
+//   (error, req, res, next) => {
+//     res.status(404).send({ error: error.message });
+//   }
+// );
 
-router.post(
-  "/uploadProfPic",
-  auth,
-  upload.single("upload"),
-  async (req, res) => {
-    const user = await User.findOne({ _id: req.user._id });
-    const pictures = await User.findOne({ _id: req.user._id }).select(
-      "profPic"
-    );
+// router.post(
+//   "/uploadProfPic",
+//   auth,
+//   upload.single("upload"),
+//   async (req, res) => {
+//     const user = await User.findOne({ _id: req.user._id });
+//     const pictures = await User.findOne({ _id: req.user._id }).select(
+//       "profPic"
+//     );
 
-    if (pictures) {
-      await User.findOneAndUpdate(
-        { _id: req.user._id },
-        { $set: { profPic: null } },
-        { new: true }
-      );
-    }
-    await User.findOneAndUpdate(
-      { _id: req.user._id },
-      { $set: { profPic: req.file.path } },
-      { new: true }
-    );
-    res.send(_.pick(user, "name", "email", "profPic"));
-  }
-);
+//     if (pictures) {
+//       await User.findOneAndUpdate(
+//         { _id: req.user._id },
+//         { $set: { profPic: null } },
+//         { new: true }
+//       );
+//     }
+//     await User.findOneAndUpdate(
+//       { _id: req.user._id },
+//       { $set: { profPic: req.file.path } },
+//       { new: true }
+//     );
+//     res.send(_.pick(user, "name", "email", "profPic"));
+//   }
+// );
 
-router.get("/getProfPic", auth, async (req, res) => {
-  const user = await User.findOne({ _id: req.user._id }).select("-password");
-  const picture = await User.findOne({ _id: user._id }).select("profPic");
+// router.get("/getProfPic", auth, async (req, res) => {
+//   const user = await User.findOne({ _id: req.user._id }).select("-password");
+//   const picture = await User.findOne({ _id: user._id }).select("profPic");
 
-  if (!user || !picture) {
-    throw new Error();
-  }
+//   if (!user || !picture) {
+//     throw new Error();
+//   }
 
-  res.json(picture);
-});
+//   res.json(picture);
+// });
 
-router.put("/removeProfPic", auth, async (req, res) => {
-  const user = await User.findOne({ _id: req.user._id });
-  if (req.body.action === "Delete") {
-    await User.findOneAndUpdate(
-      { _id: user._id },
-      { $set: { profPic: null } },
-      { new: true }
-    );
-  }
+// router.put("/removeProfPic", auth, async (req, res) => {
+//   const user = await User.findOne({ _id: req.user._id });
+//   if (req.body.action === "Delete") {
+//     await User.findOneAndUpdate(
+//       { _id: user._id },
+//       { $set: { profPic: null } },
+//       { new: true }
+//     );
+//   }
 
-  res.send(_.pick(user, "name", "email"));
-});
+//   res.send(_.pick(user, "name", "email"));
+// });
 
-module.exports = router;
+// module.exports = router;
--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use("/Image", express.static("Image"));
+// app.use("/Image", express.static("Image"));
 const keys = require("./Config/keys");
 const user = require("./Routes/user");
 const auth = require("./Routes/auth");
 const friend = require("./Routes/friends");
-const image = require("./Routes/image");
+// const image = require("./Routes/image");
 
 mongoose
   .connect(keys.mongoURI)
@@ -38,6 +38,6 @@ app.use(express.json());
 app.use("/api/user", user);
 app.use("/api/user", auth);
 app.use("/api/friend", friend);
-app.use("/api/image", image);
+// app.use("/api/image", image);
 
 module.exports = server;
